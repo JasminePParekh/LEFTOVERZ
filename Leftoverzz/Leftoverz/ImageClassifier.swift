b1b99ffe2b22574c5227e52837196ccd265803e7
Jasmine Parekh <jasmine.p.parekh@gmail.com>
2023-04-24 19:29:24 -0400

    FOR U U FOCK

---
+++ Leftoverzz/Leftoverz/ImageClassifier.swift
@@ -0,0 +1,58 @@
+//
+//  ImageClassifier.swift
+//  Leftoverz
+//
+//  Created by Jasmine Parekh on 4/18/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreML
+import Vision
+import CoreImage
+
+class ImageClassifier: ObservableObject {
+    
+    @Published private var classifier = Classifier()
+    
+    var imageClass: String? {
+        classifier.results
+    }
+        
+    // MARK: Intent(s)
+    func detect(uiImage: UIImage) {
+        guard let ciImage = CIImage (image: uiImage) else { return }
+        classifier.detect(ciImage: ciImage)
+        
+    }
+        
+}
+
+struct Classifier {
+    
+    private(set) var results: String?
+    
+    mutating func detect(ciImage: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: MobileNetV2(configuration: MLModelConfiguration()).model)
+        else {
+            return
+        }
+         
+        let request = VNCoreMLRequest(model: model)
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        
+        try? handler.perform([request])
+        
+        guard let results = request.results as? [VNClassificationObservation] else {
+            return
+        }
+        
+        if let firstResult = results.first {
+            self.results = firstResult.identifier
+        }
+        
+    }
+    
+}
---
+++ Leftoverzz/Leftoverz/LeftoverzApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftoverzApp.swift
+//  Leftoverz
+//
+//  Created by Jasmine Parekh on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LeftoverzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(classifier: ImageClassifier())
+        }
+    }
+}
