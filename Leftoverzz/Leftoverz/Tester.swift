43751056c603d33fb22afde2a5297fe8e316d724
Johann Antisseril <johann@terpmail.umd.edu>
2023-04-26 19:50:12 -0400

    Reworked Camera View. Didn't Edit ImagePicker.

---
+++ Leftoverzz/Leftoverz/Tester.swift
@@ -0,0 +1,38 @@
+//
+//  Tester.swift
+//  Leftoverz
+//
+//  Created by Johann Antisseril on 4/26/23.
+//
+
+import Foundation
+import SwiftUI
+
+//struct TodoItem: Identifiable {
+//    let id: UUID
+//    var title: String
+//}
+//
+//struct TodoList: View {
+//    @State var items = [TodoItem(id: UUID(), title: "test1"), TodoItem(id: UUID(), title: "test2"), TodoItem(id: UUID(), title: "test3")]
+//
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                List {
+//                    ForEach($items) { $item in
+//                        TextField("Title", text: $item.title)
+//                    }
+//                }
+//            }
+//            .navigationTitle("Todo list")
+//            .toolbar { EditButton() }
+//        }
+//    }
+//}
+//
+//struct TodoList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TodoList()
+//    }
+//}
